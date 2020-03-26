7da93db08938a4d77edadc79704e6469fc94494b
ivopavlov <pavlov.ivo@gmail.com>
2020-03-26 16:55:54 -0700

    user creation with validations, postman tested

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,17 +1,29 @@
 class Api::UsersController < ApplicationController
 
+  # remove before deploy/production
+  skip_before_action :verify_authenticity_token
+
   def create
     @user = User.new(user_params)
 
     if @user.save
-      login(@user)
-      render "api/users/show"
+      # login(@user)
+      # render "api/users/show"
+      render json: @user
     else
       render json: @user.errors.full_messages, status: 422
     end
   end
 
   def show
+    @user = User.find(params[:id])
+
+    if @user
+      # render :show
+      render json: "showing a user"
+    else
+      render json: @user.errors.full_messages, status: 404
+    end
 
   end
 
@@ -21,7 +33,7 @@ class Api::UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :name, :email, :password, :bio)
+    params.require(:user).permit(:username, :name, :email, :password, :bio, :login_input)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,38 @@
-class User < ApplicationRecord
+load 'validation_helpers.rb'
 
-  require 'validation_helpers'
-
+class User < ApplicationRecord  
+  
   attr_reader :password
 
+  # check make sure username, name, email, password_digest, and session_token are present
   validates :username, :name, :email, :password_digest, :session_token, presence: true
-  validates :username, :email, uniqueness: true
-  validates :username, length: { minimum: 6, maximum: 30 }, valid_username: true
+
+  # check to make sure username and email have not been registered
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: { case_sensitive: false }
+
+  # check to make sure username is at least 6 chars, and at most 30, and is only allowed chars
+  # valid_username is custom validation method
+  validates :username, length: { minimum: 4, maximum: 30 }, valid_username: true
+
+  # enforces name length between 2 and 50 chars
   validates :name, length: { minimum: 2, maximum: 50 }
+
+  # check to make sure email is valid, valid_email is custom validation method
   validates :email, valid_email: true
+
+  # check to make sure password is between 8 and 30 chars, secure_password is
+  # a custom validation method, allow_nil true is because password itself is
+  # not store on DB
   validates :password, length: { minimum: 8, maximum: 30 }, secure_password: true, allow_nil: true
+
+  # check to make sure user bio is not longer than 255 chars,
+  # allow_nil is true because the bio is optional
   validates :bio, length: { maximum: 255 }, allow_nil: true
 
   after_initialize :ensure_session_token
+
+  before_validation :strip_email
 
   def self.find_by_credentials(login_input, password)
     user = login_input.include?("@") ? User.find_by(email: login_input) :
@@ -21,18 +41,23 @@ class User < ApplicationRecord
     user && user.is_password?(password) ? user : nil
   end
 
-  # def email=(email)
-  #   parts = email.split("@")
-  #   parts[0].gsub!('.','')
-  #   parts[0] = parts[0].split('+')[0]
-  #   @email = parts.join('@')
-  # end
+  # cleans up entered email address to remove any tag(s)
+  def strip_email
+    parts = self.email.split("@")
+    parts[0].gsub!('.','')
+    parts[0] = parts[0].split('+')[0]
+    self.email = parts.join('@')
+  end
 
+  # hashes password upon creation and adds it to password_digest column
   def password=(password)
     @password = password
+    p "self - password"
+    p self
     self.password_digest = BCrypt::Password.create(password)
   end
 
+  # verification of password
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end

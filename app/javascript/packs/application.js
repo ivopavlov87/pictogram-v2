d56b3ce049f8cecb5116121e1661987bd2ba17cc
ivopavlov <pavlov.ivo@gmail.com>
2020-04-09 19:54:18 -0700

    favicon addition

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,5 +15,7 @@ require("jquery-ui");
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
+// original is first:
 // const images = require.context('../images', true)
+// const images = require.context('../../assets/images', true)
 // const imagePath = (name) => images(name, true)

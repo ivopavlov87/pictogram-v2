698866019861c987efc8c3bce42ecf40362f73e3
ivopavlov <pavlov.ivo@gmail.com>
2020-04-14 14:14:03 -0700

    possible AWS S3 upload fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,10 +35,11 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  config.active_storage.service = :amazon_prod
-
+  
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
+
+  config.active_storage.service = :amazon_prod
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
